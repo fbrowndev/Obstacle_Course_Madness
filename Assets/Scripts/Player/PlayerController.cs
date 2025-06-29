7dd802a4637a28c90aad9ca74e0abf831cfed1f4
Fran-Kee Brown <106333841+fbrowndev@users.noreply.github.com>
2025-06-28 20:55:31 -0400

    Player Movement implemented

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Player Variables
+    [Header("General Settings")]
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _jumpHeight = 1.5f;
+    [SerializeField] private float _gravity = -9.81f;
+
+
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    private bool _isGrounded;
+
+    [Header("Jump Settings")]
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundDistance = 0.4f;
+    [SerializeField] private LayerMask _groundMask;
+
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerJump();
+        PlayerGravity();
+    }
+
+    #region Player Movement
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        _controller.Move(_moveSpeed * Time.deltaTime * move);
+    }
+
+    void PlayerJump()
+    {
+        //Ground Check first
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
+        if(_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+        
+    }
+
+    void PlayerGravity()
+    {
+        _velocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    #endregion
+}
